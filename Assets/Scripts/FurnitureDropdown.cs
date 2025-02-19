799aeb7ae19c19909aa199f57d2f483c23ade30f
zrei <88964793+zrei@users.noreply.github.com>
2025-02-19 17:48:23 +0800

    Update UI

--- Assets/Scripts/FurnitureDropdown.cs
+++ Assets/Scripts/FurnitureDropdown.cs
@@ -1,16 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FurnitureDropdown : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public struct FurnitureButton
     {
-        
+        public Sprite FurnitureSprite;
+        public GameObject FurniturePrefab;
+    }
+    [SerializeField] private RectTransform m_RectTransform;
+    [SerializeField] private float m_FinalPosY;
+    [SerializeField] private float m_FinalHeight;
+    [SerializeField] private float m_TotalTime;
+    [SerializeField] private Button m_MenuButtonPrefab;
+
+    [SerializeField] private FurnitureButton[] m_FurnitureButtons;
+
+    private Coroutine m_CurrPlayingCoroutine = null;
+
+    public GameObject CurrSelectedFurniturePrefab {get; private set;} = null;
+
+    private List<Button> m_MenuButtons = new();
+
+    private void Start()
+    {
+        foreach (FurnitureButton furnitureButton in m_FurnitureButtons)
+        {
+            Button menuButton = Instantiate(m_MenuButtonPrefab, this.transform);
+            menuButton.onClick.AddListener(() => OnSelectFurniture(menuButton, furnitureButton));
+            m_MenuButtons.Add(menuButton);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        foreach (Button menuButton in m_MenuButtons)
+        {
+            menuButton.onClick.RemoveAllListeners();
+        }
+    }
+
+    private void OnSelectFurniture(Button button, FurnitureButton furnitureButton)
+    {
+        button.Select();
+        CurrSelectedFurniturePrefab = furnitureButton.FurniturePrefab;
+    }
+
+    public void ToggleDropdown(bool expand)
+    {
+        if (m_CurrPlayingCoroutine != null)
+        {
+            StopCoroutine(m_CurrPlayingCoroutine);
+        }
+
+        m_CurrPlayingCoroutine = StartCoroutine(ToggleDropdownCoroutine(expand));
+    }
+
+    private IEnumerator ToggleDropdownCoroutine(bool expand)
+    {
+        float targetPos = expand ? m_FinalPosY : 0;
+        float targetHeight = expand ? m_FinalHeight : 0;
+        float time = Mathf.Abs(targetPos - m_RectTransform.anchoredPosition.y) / m_FinalPosY * m_TotalTime;
+
+        float t = 0f;
+        while (t < time)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            m_RectTransform.anchoredPosition = new Vector2(m_RectTransform.anchoredPosition.x, Mathf.Lerp(m_RectTransform.anchoredPosition.y, targetPos, t / time));
+            m_RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Lerp(m_RectTransform.rect.height, targetHeight, t / time));
+        }
     }
 }
--- Assets/Scripts/MenuButtonHandler.cs
+++ Assets/Scripts/MenuButtonHandler.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlayState
+{
+    NONE,
+    PLACE_FURNITURE,
+    RELOCATE_FURNITURE,
+    DELETE_FURNITURE
+}
 
 public class MenuButtonHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public struct PlayStateButton
     {
-        
+        public Button Button;
+        public PlayState PlayState;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private PlayStateButton[] m_PlayStateButtons;
+    [SerializeField] private FurnitureDropdown m_FurnitureDropdown;
+
+    public PlayState CurrPlayState {get; private set;} = PlayState.NONE;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
     {
-        
+        foreach (PlayStateButton playStateButton in m_PlayStateButtons)
+        {
+            playStateButton.Button.onClick.AddListener(() => OnPlayStateButtonPressed(playStateButton));
+        }
+    }
+
+    private void Oestroy()
+    {
+        foreach (PlayStateButton playStateButton in m_PlayStateButtons)
+        {
+            playStateButton.Button.onClick.RemoveAllListeners();
+        }
+    }
+
+    private void OnPlayStateButtonPressed(PlayStateButton playStateButton)
+    {
+        if (CurrPlayState == playStateButton.PlayState)
+            return;
+
+        playStateButton.Button.Select();
+
+        if (CurrPlayState == PlayState.PLACE_FURNITURE)
+        {
+            // compress dropdown
+        }
+
+        CurrPlayState = playStateButton.PlayState;
+
+        if (CurrPlayState == PlayState.PLACE_FURNITURE)
+        {
+            // expand dropdown
+        }
     }
 }
